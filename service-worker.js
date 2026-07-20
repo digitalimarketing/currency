67fabd8f58cf4164a95caa0e2eeda07b75d55f8b
digitalimarketing <digitalimarketingchannel@gmail.com>
2026-07-20 18:59:41 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,1 @@
-const CACHE = "euro-only-v1";
-const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-});
-self.addEventListener("fetch", (e) => {
-  if (ASSETS.some(a => e.request.url.endsWith(a.replace("./","")))) {
-    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
-  }
-});
+self.addEventListener("install",()=>self.skipWaiting()); self.addEventListener("activate",(e)=>e.waitUntil(self.clients.claim())); self.addEventListener("fetch",()=>{});
